569a55e1bb92e3604ea912d51f5b08ef2298b5cb
Yousif Hamid <yousif.hamid@gmail.com>
2026-02-25 00:20:26 +0200

    Cleanup and force fresh build

--- test_hash.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcryptjs';
-async function test() {
-    const isMatch = await bcrypt.compare('SM@11223344', '$2b$10$PMHGi/N2IQrt3EaX/0cFxuYKXGbyQbvDFUmBxztMZ5MW0h2hC2SfW');
-    console.log({ isMatch });
-}
-test();
